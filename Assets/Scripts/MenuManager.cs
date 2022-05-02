4eccbddb0664d2cc3eedb39c3278558424dcc296
alperarslanturk <alperars19@gmail.com>
2022-05-02 17:35:42 +0300

    between scenes
    Data persistence between scenes done.

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+   public static MenuManager Instance;
+
+   public string PlayerName;
+
+   private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MenuUIHandler : MonoBehaviour
+{
+    SceneManager sceneManager;
+    public InputField PlayerNameField;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+
+        if(UnityEditor.EditorApplication.isPlaying)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+    }
+
+    public void PlayerNameEntered()
+    {
+        MenuManager.Instance.PlayerName = PlayerNameField.text;
+    }
+}
